67acf63260a60b067cbbecb55289e1337f7b427b
Mariyana Boyadzhieva <m.boyadzhieva99@gmail.com>
2022-06-05 11:42:46 +0300

    Expenses and incomes added to User

--- FamilyBudgetTracker/FBT.WebAPI/Data/Models/User.cs
+++ FamilyBudgetTracker/FBT.WebAPI/Data/Models/User.cs
@@ -1,12 +1,17 @@
 ﻿namespace FBT.WebAPI.Data.Models
 {
     using Microsoft.AspNetCore.Identity;
+    using System.Collections.Generic;
 
     public class User : IdentityUser 
     {
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public IEnumerable<Expense> Expenses { get; } = new HashSet<Expense>();
+
+        public IEnumerable<Income> Incomes { get; } = new HashSet<Income>();
 
     }
 }

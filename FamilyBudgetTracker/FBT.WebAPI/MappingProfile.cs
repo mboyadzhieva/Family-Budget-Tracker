ee114b72f111a63aaff7057585ac072db35c5475
Mariyana Boyadzhieva <m.boyadzhieva99@gmail.com>
2022-06-06 00:23:28 +0300

    Mapping configuration updated

--- FamilyBudgetTracker/FBT.WebAPI/MappingProfile.cs
+++ FamilyBudgetTracker/FBT.WebAPI/MappingProfile.cs
@@ -2,6 +2,10 @@
 {
     using AutoMapper;
     using Data.Models;
+    using FBT.WebAPI.Features.Expenses;
+    using FBT.WebAPI.Features.Incomes;
+    using FBT.WebAPI.Features.RecurringExpenses;
+    using FBT.WebAPI.Features.RecurringIncomes;
     using Features.Identity;
 
     public class MappingProfile : Profile
@@ -10,6 +14,18 @@
         {
             CreateMap<User, RegisterRequestModel>();
             CreateMap<RegisterRequestModel, User>();
+
+            CreateMap<Expense, ExpenseRequestModel>();
+            CreateMap<ExpenseRequestModel, Expense>();
+
+            CreateMap<Income, IncomeRequestModel>();
+            CreateMap<IncomeRequestModel, Income>();
+
+            CreateMap<RecurringExpense, RecurringExpenseRequestModel>();
+            CreateMap<RecurringExpenseRequestModel, RecurringExpense>();
+
+            CreateMap<RecurringIncome, RecurringIncomeRequestModel>();
+            CreateMap<RecurringIncomeRequestModel, RecurringIncome>();
         }
     }
 }

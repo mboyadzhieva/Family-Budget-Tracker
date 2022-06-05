188577f06bfe7bf2b7971569d8f866492cbbfcf7
Mariyana Boyadzhieva <m.boyadzhieva99@gmail.com>
2022-06-05 11:43:13 +0300

    Expenses and incomes added to Database context

--- FamilyBudgetTracker/FBT.WebAPI/Data/FamilyBudgetTrackerDbContext.cs
+++ FamilyBudgetTracker/FBT.WebAPI/Data/FamilyBudgetTrackerDbContext.cs
@@ -6,10 +6,31 @@
 
     public class FamilyBudgetTrackerDbContext : IdentityDbContext<User>
     {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public DbSet<Income> Incomes { get; set; }
+
         public FamilyBudgetTrackerDbContext(
             DbContextOptions<FamilyBudgetTrackerDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<Expense>()
+                .HasOne(e => e.User)
+                .WithMany(u => u.Expenses)
+                .HasForeignKey(e => e.UserId);
+
+            builder
+                .Entity<Income>()
+                .HasOne(i => i.User)
+                .WithMany(u => u.Incomes)
+                .HasForeignKey(i => i.UserId);
+
+            base.OnModelCreating(builder);
         }
     }
 }
